8a0742ad73f357736caaa0088537b2f484ae83d6
Noureddine <Bouzire@DESKTOP-MP90IJO>
2024-04-21 20:15:57 +0200

    Implement the logic of application

--- src/main/java/dao/ReservationDAO.java
+++ src/main/java/dao/ReservationDAO.java
@@ -1,5 +1,9 @@
-package dao;
+package DAO;
+import model.Reservation;
+import java.util.List;
 
 public interface ReservationDAO {
-
+    List<Reservation> getReservations();
+    void createReservation(Reservation reservation);
+    void cancelReservation(int reservationId);
 }
--- src/main/java/dao/RoomDAO.java
+++ src/main/java/dao/RoomDAO.java
@@ -1,5 +1,11 @@
-package dao;
+package DAO;
+
+import java.util.List;
+
+import model.Room;
 
 public interface RoomDAO {
-
+	
+	 List<Room> showRoom();
+	List<Room> searchRoom(String searchTerm);
 }
